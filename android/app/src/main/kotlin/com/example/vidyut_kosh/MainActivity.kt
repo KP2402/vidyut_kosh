5a97175dc4f04a806a0f3f8c046155122d5683c3
Prashant Kilari <k.prashant89@gmail.com>
2023-04-02 12:55:52 +0530

    Vidyut Kosh first commit

---
+++ android/app/src/main/kotlin/com/example/vidyut_kosh/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.vidyut_kosh
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,98 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Vidyut Kosh',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightGreenAccent),
+        ),
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyAppState extends ChangeNotifier {
+  var current = WordPair.random();
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var pair = appState.current;
+    final theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!.copyWith(
+        color: theme.colorScheme.primary,
+        fontStyle: FontStyle.normal,
+        fontWeight: FontWeight.normal
+    );
+
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Vidyut Kosh word:', style: style,),
+            BigCard(pair: pair),
+            SizedBox(height: 10),
+
+            ElevatedButton(onPressed: () {
+              print('button pressed!');
+              appState.getNext();
+            }, child: Text('Next')),
+          ],
+        ),
+      ),
+      );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+      fontStyle: FontStyle.normal,
+      fontWeight: FontWeight.bold
+    );
+    return Card(
+      color: theme.colorScheme.primary,
+      elevation: 25,
+      shadowColor: Colors.deepOrangeAccent,
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Text(
+            "${pair.first} ${pair.second}",
+            style: style
+        ),
+      ),
+    );
+    // return Text(pair.asCamelCase);
+  }
+}
